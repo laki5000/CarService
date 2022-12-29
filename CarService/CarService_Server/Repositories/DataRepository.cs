380831fab1e857f931c67a2eae93d8716b79c592
laki5000 <78901688+laki5000@users.noreply.github.com>
2022-12-29 20:58:04 +0100

    Add files via upload

--- CarService/CarService_Server/Repositories/DataRepository.cs
+++ CarService/CarService_Server/Repositories/DataRepository.cs
@@ -1,4 +1,5 @@
 ﻿using CarService_Common.Models;
+using Microsoft.VisualBasic;
 using System.Collections.Generic;
 using System.IO;
 using System.Text.Json;
@@ -25,6 +26,49 @@ namespace CarService_Server.Repositories
         {
             var rawData = JsonSerializer.Serialize(data);
             File.WriteAllText(filename, rawData);
+        }
+
+        public static double CalculateETA(Data data)
+        {
+            double ETA = 1;
+            //vissza adja a mostani időt
+            int CarAge = DateAndTime.Now.Year - data.ManufactureYear;
+
+            double[] Severity = { 0.2, 0.2, 0.4, 0.4, 0.6, 0.6, 0.8, 0.8, 1, 1 };
+
+            switch (data.WorkCategory)
+            {
+                case "Chassis":
+                    ETA = 3;
+                    break;
+                case "Engine":
+                    ETA = 8;
+                    break;
+                case "Suspension":
+                    ETA = 6;
+                    break;
+                case "Brake":
+                    ETA = 4;
+                    break;
+            }
+
+            if (CarAge >= 0 && CarAge <= 5)
+            {
+                ETA *= 0.5;
+            }
+            if (CarAge >= 10 && CarAge <= 20)
+            {
+                ETA *= 1.5;
+            }
+            if (CarAge > 20)
+            {
+                ETA *= 2;
+            }
+
+            ETA *= Severity[data.Seriousness];
+
+            return ETA;
+
         }
     }
 }
