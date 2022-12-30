d066c0acf95a095b2b63cd08fea8ad6880ed6527
laki5000 <78901688+laki5000@users.noreply.github.com>
2022-12-30 15:42:44 +0100

    Add files via upload

---
+++ CarService/CarService_Server/Controllers/DataController.cs
@@ -0,0 +1,101 @@
+﻿using CarService_Common.Models;
+using CarService_Server.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CarService_Server.Controllers
+{
+    [ApiController]
+    [Route("api/data")]
+    public class DataController : Controller
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<Data>> Get()
+        {
+            var data = DataRepository.GetData();
+            return Ok(data);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Data> Get(int id)
+        {
+            var datas = DataRepository.GetData();
+
+            var data = datas.FirstOrDefault(x => x.Id == id);
+            if (data != null)
+            {
+                return Ok(data);
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Data data)
+        {
+            var datas = DataRepository.GetData().ToList();
+
+            data.Id = GetNewId(datas);
+            datas.Add(data);
+
+            DataRepository.StoreData(datas);
+            return Ok();
+        }
+
+        [HttpPut]
+        public ActionResult Put([FromBody] Data data)
+        {
+            var datas = DataRepository.GetData().ToList();
+
+            var dataToUpdate = datas.FirstOrDefault(p => p.Id == data.Id);
+            if (dataToUpdate != null)
+            {
+                dataToUpdate.Name = data.Name;
+                dataToUpdate.Type = data.Type;
+                dataToUpdate.PlateNumber = data.PlateNumber;
+                dataToUpdate.ManufactureYear = data.ManufactureYear;
+                dataToUpdate.WorkCategory = data.WorkCategory;
+                dataToUpdate.Description = data.Description;
+                dataToUpdate.Seriousness = data.Seriousness;
+
+                DataRepository.StoreData(datas);
+                return Ok();
+            }
+
+            return NotFound();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            var datas = DataRepository.GetData().ToList();
+
+            var dataToDelete = datas.FirstOrDefault(p => p.Id == id);
+            if (dataToDelete != null)
+            {
+                datas.Remove(dataToDelete);
+
+                DataRepository.StoreData(datas);
+                return Ok();
+            }
+
+            return NotFound();
+        }
+
+        private long GetNewId(IEnumerable<Data> datas)
+        {
+            long newId = 0;
+            foreach (var data in datas)
+            {
+                if (newId < data.Id)
+                {
+                    newId = data.Id;
+                }
+            }
+
+            return newId + 1;
+        }
+    }
+}
---
+++ CarService/CarService_Server/Repositories/DataRepository.cs
@@ -0,0 +1,74 @@
+﻿using CarService_Common.Models;
+using Microsoft.VisualBasic;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace CarService_Server.Repositories
+{
+    public static class DataRepository
+    {
+        private const string filename = "alldata.json";
+
+        public static IEnumerable<Data> GetData()
+        {
+            if (File.Exists(filename))
+            {
+                var rawData = File.ReadAllText(filename);
+                var data = JsonSerializer.Deserialize<IEnumerable<Data>>(rawData);
+                return data;
+            }
+
+            return new List<Data>();
+        }
+
+        public static void StoreData(IEnumerable<Data> data)
+        {
+            var rawData = JsonSerializer.Serialize(data);
+            File.WriteAllText(filename, rawData);
+        }
+
+        public static double CalculateETA(Data data)
+        {
+            double ETA = 1;
+            //vissza adja a mostani időt
+            int CarAge = DateAndTime.Now.Year - data.ManufactureYear;
+
+            double[] Severity = { 0.2, 0.2, 0.4, 0.4, 0.6, 0.6, 0.8, 0.8, 1, 1 };
+
+            switch (data.WorkCategory)
+            {
+                case "Chassis":
+                    ETA = 3;
+                    break;
+                case "Engine":
+                    ETA = 8;
+                    break;
+                case "Suspension":
+                    ETA = 6;
+                    break;
+                case "Brake":
+                    ETA = 4;
+                    break;
+            }
+
+            if (CarAge >= 0 && CarAge <= 5)
+            {
+                ETA *= 0.5;
+            }
+            if (CarAge >= 10 && CarAge <= 20)
+            {
+                ETA *= 1.5;
+            }
+            if (CarAge > 20)
+            {
+                ETA *= 2;
+            }
+
+            ETA *= Severity[data.Seriousness];
+
+            return ETA;
+
+        }
+    }
+}
